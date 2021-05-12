f587b15ceb9af69627f4fa91629a706a10e80202
Chang Chen Chien <e850506@gmail.com>
2021-05-12 19:32:33 +0800

    Setup error handling helper function

--- cmd/api/tasks.go
+++ cmd/api/tasks.go
@@ -19,7 +19,7 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 func (app *application) showTaskHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -35,7 +35,6 @@ func (app *application) showTaskHandler(w http.ResponseWriter, r *http.Request) 
 	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"task": task}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
