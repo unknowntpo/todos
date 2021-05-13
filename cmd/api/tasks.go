b2cd72996fa6a5ec251b3b6a1475201b84abeac1
Chang Chen Chien <e850506@gmail.com>
2021-05-13 13:15:31 +0800

    Managing bad request by triaging the decode error

--- cmd/api/tasks.go
+++ cmd/api/tasks.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -18,9 +17,9 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 		Done    bool   `json:done"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
