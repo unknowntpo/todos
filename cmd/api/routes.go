ef04455e77f414b84cde60618067cd14dab73a35
Chang Chen Chien <e850506@gmail.com>
2021-05-21 19:40:50 +0800

    Chapter 13.3. Registering a User

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/tasks/:id", app.updateTaskHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/tasks/:id", app.deleteTaskHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
