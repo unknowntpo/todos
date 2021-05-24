2696617d71325497c57d675a004248d36210073f
Chang Chen Chien <e850506@gmail.com>
2021-05-24 17:02:09 +0800

    Use the background goroutine to send the emails

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -162,3 +162,17 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 	// Otherwise, return the converted integer value.
 	return i
 }
+
+// background accepts an arbitrary function as a parameter.
+func (app *application) background(fn func()) {
+	go func() {
+		// Recover any panic.
+		defer func() {
+			if err := recover(); err != nil {
+				app.logger.PrintError(fmt.Errorf("%s", err), nil)
+			}
+		}()
+
+		fn()
+	}()
+}
