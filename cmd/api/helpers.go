95a338a92d66f844304540efd647c7c2fc78c823
Chang Chen Chien <e850506@gmail.com>
2021-05-24 17:14:44 +0800

    Gracefful shutdown of background tasks

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -165,7 +165,10 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int, v *
 
 // background accepts an arbitrary function as a parameter.
 func (app *application) background(fn func()) {
+	app.wg.Add(1)
 	go func() {
+		defer app.wg.Done()
+
 		// Recover any panic.
 		defer func() {
 			if err := recover(); err != nil {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/data"
@@ -47,6 +48,7 @@ type application struct {
 	logger *jsonlog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
