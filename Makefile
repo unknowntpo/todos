279c9fd496b9f2096b0d74fcaeaf07e31abe595f
Chang Chen Chien <e850506@gmail.com>
2021-05-31 11:29:58 +0800

    Displaying the version number and build time

--- Makefile
+++ Makefile
@@ -72,10 +72,12 @@ vendor:
 # BUILD
 # ==================================================================================== #
 
+current_time = $(shell date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 ## build/api: build the cmd/api application
 .PHONY: build/api
 build/api:
 	@echo 'Building cmd/api...'
-	go build -ldflags='-s' -o=./bin/api ./cmd/api
-	GOOS=linux GOARCH=amd64 go build -ldflags='-s' -o=./bin/linux_amd64/api ./cmd/api
-	GOOS=darwin GOARCH=amd64 go build -ldflags='-s' -o=./bin/darwin_amd64/api ./cmd/api
+	go build -ldflags='-s -X main.buildTime=${current_time}' -o=./bin/api ./cmd/api
+	GOOS=linux GOARCH=amd64 go build -ldflags='-s -X main.buildTime=${current_time}' -o=./bin/linux_amd64/api ./cmd/api
+	GOOS=darwin GOARCH=amd64 go build -ldflags='-s -X main.buildTime=${current_time}' -o=./bin/darwin_amd64/api ./cmd/api
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"os"
 	"runtime"
 	"strings"
@@ -20,6 +21,10 @@ import (
 
 // version contains hard-coded version number
 const version = "1.0.0"
+
+// buildTime holds the executable binary build time. Note that this
+// must be a string type, as the -X linker flag will only work with string variables.
+var buildTime string
 
 // config holds configuration of server
 type config struct {
@@ -90,7 +95,17 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	// If the version flag value is true, then print out the version number and
+	// immediately exit.
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		fmt.Printf("Build time:\t%s\n", buildTime)
+		os.Exit(0)
+	}
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
