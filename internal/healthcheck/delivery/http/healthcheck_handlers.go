47511848a935811d3efc481e0c7ba90513aec969
Chang Chen Chien <e850506@gmail.com>
2021-06-10 08:37:45 +0800

    Implement test for /v1/healthcheck
    TODO: Display metrics.
    TODO: Imitate go-rest-api and create testing helpers for api endpoint.

--- internal/healthcheck/delivery/http/healthcheck_handlers.go
+++ internal/healthcheck/delivery/http/healthcheck_handlers.go
@@ -5,13 +5,16 @@ import (
 	"net/http"
 )
 
+// HealthcheckDelivery represent delivery layer of healcheck api endpoint.
 type HealthcheckDelivery struct{}
 
+// NewHealthcheckDelivery registers all handlers in /v1/healcheck to the router.
 func NewHealthcheckDelivery(router *httprouter.Router) {
 	handler := &HealthcheckDelivery{}
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", handler.HealthcheckHandler)
 }
 
+// TODO: Display metrics.
 func (h *HealthcheckDelivery) HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
