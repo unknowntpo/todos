e2389c1b37a5aee3d4ac9920e22654e59b934417
Chang Chen Chien <e850506@gmail.com>
2021-07-22 09:56:09 +0800

    Remove unnessary comment at logrus package

--- internal/logger/logrus/logrus.go
+++ internal/logger/logrus/logrus.go
@@ -34,12 +34,8 @@ func (lw *logrusWrapper) PrintInfo(message string, properties map[string]string)
 }
 
 func (lw *logrusWrapper) PrintError(err error, properties map[string]string) {
-	//_, caller, line, _ := runtime.Caller(1)
 	lw.Logger.WithFields(logrus.Fields{
 		"properties": properties,
-		//"trace":      string(debug.Stack()),
-		//"caller":     fmt.Sprintf("%s:%d", caller, line),
-		//"trace": fmt.Sprintf("%+v", err),
 	}).Error(err)
 }
 
