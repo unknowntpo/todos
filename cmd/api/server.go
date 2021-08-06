cdac2c346516b8ead657b1527369b93600aa0123
Chang Chen Chien <e850506@gmail.com>
2021-08-06 08:44:03 +0800

    Implement graceful shutdown
    TODO: Wait for background goroutine to be completed.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/logger"
@@ -17,12 +22,53 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	shutdownErr := make(chan error)
+
+	go func() {
+		// Why we need buffered channel ?
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+		logger.Log.PrintInfo("shutting down server", map[string]string{
+			"signal": s.String(),
+		})
+
+		// do shutdown routine.
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownErr <- err
+		}
+
+		// TODO: wait for background task (e.g. email sender) to be completed.
+		shutdownErr <- nil
+	}()
+
 	logger.Log.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
 	})
 
 	err := srv.ListenAndServe()
+	if err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			// Don't need to do graceful shutdown process, just return the error.
+			return err
+		}
+	}
 
-	return err
+	err = <-shutdownErr
+	if err != nil {
+		return err
+	}
+
+	logger.Log.PrintInfo("stopped server", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
 }
