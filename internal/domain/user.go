994b3474b6b9f350444b8519086ab66cb1122255
Chang Chen Chien <e850506@gmail.com>
2021-08-18 14:42:07 +0800

    Move definition of password from domain to helpers/password

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -2,24 +2,19 @@ package domain
 
 import (
 	"time"
+
+	"github.com/unknowntpo/todos/internal/helpers/password"
 )
 
 // User represents an individual user.
 type User struct {
-	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  password  `json:"-"`
-	Activated bool      `json:"activated"`
-	Version   int       `json:"-"`
-}
-
-// password is a struct containing the plaintext and hashed
-// versions of the password for a user.
-type password struct {
-	plaintext *string
-	hash      []byte
+	ID        int64             `json:"id"`
+	CreatedAt time.Time         `json:"created_at"`
+	Name      string            `json:"name"`
+	Email     string            `json:"email"`
+	Password  password.Password `json:"-"`
+	Activated bool              `json:"activated"`
+	Version   int               `json:"-"`
 }
 
 type UserUsecase interface{}
