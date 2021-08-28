7a9cb63c0a9d3e717acd501955049fdcaea56f04
Chang Chen Chien <e850506@gmail.com>
2021-08-28 09:21:15 +0800

    Move configuration code to seperate file

---
+++ cmd/api/config.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+// config holds configuration of server
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
+}
+
+// config set the configuration and  returns the config struct.
+func setConfig() config {
+	var cfg config
+
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	// db setup
+	// Use the empty string "" as the default value for the db-dsn command-line flag,
+	// let Makefile specify it explicitly.
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
+
+	// Read the connection pool settings from command-line flags into the config struct.
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	flag.Parse()
+
+	// If the version flag value is true, then print out the version number and
+	// immediately exit.
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		fmt.Printf("Build time:\t%s\n", buildTime)
+		os.Exit(0)
+	}
+
+	return cfg
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,9 +30,7 @@ package main
 import (
 	"context"
 	"database/sql"
-	"flag"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/logger"
@@ -47,18 +45,6 @@ var (
 	version   string
 	buildTime string
 )
-
-// config holds configuration of server
-type config struct {
-	port int
-	env  string
-	db   struct {
-		dsn          string
-		maxOpenConns int
-		maxIdleConns int
-		maxIdleTime  string
-	}
-}
 
 // application holds the dependencies for our HTTP handlers, helpers, and middleware.
 type application struct {
@@ -91,38 +77,6 @@ func main() {
 	if err != nil {
 		logger.Log.PrintFatal(fmt.Errorf("server error: %v", err), nil)
 	}
-}
-
-// config set the configuration and  returns the config struct.
-func setConfig() config {
-	var cfg config
-
-	displayVersion := flag.Bool("version", false, "Display version and exit")
-
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-
-	// db setup
-	// Use the empty string "" as the default value for the db-dsn command-line flag,
-	// let Makefile specify it explicitly.
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "PostgreSQL DSN")
-
-	// Read the connection pool settings from command-line flags into the config struct.
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
-	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
-
-	flag.Parse()
-
-	// If the version flag value is true, then print out the version number and
-	// immediately exit.
-	if *displayVersion {
-		fmt.Printf("Version:\t%s\n", version)
-		fmt.Printf("Build time:\t%s\n", buildTime)
-		os.Exit(0)
-	}
-
-	return cfg
 }
 
 func openDB(cfg config) (*sql.DB, error) {
