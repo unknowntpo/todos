11be162adf10eda91b8791ce7a96e7d64fbcd749
Chang Chen Chien <e850506@gmail.com>
2021-08-28 09:27:46 +0800

    mock task usecase

--- internal/domain/mock/task_usecase.go
+++ internal/domain/mock/task_usecase.go
@@ -4,63 +4,43 @@ import (
 	"context"
 
 	"github.com/unknowntpo/todos/internal/domain"
-	"github.com/unknowntpo/todos/internal/helpers"
+
+	"github.com/stretchr/testify/mock"
 )
 
-type MockTaskUsecase struct{}
+type MockTaskUsecase struct {
+	mock.Mock
+}
 
 func NewTaskUsecase() domain.TaskUsecase {
 	return &MockTaskUsecase{}
 }
 
 func (m *MockTaskUsecase) GetAll(ctx context.Context, title string, filters domain.Filters) ([]*domain.Task, domain.Metadata, error) {
-	return nil, &helpers.Metadata{}, nil
+	args := m.Called(ctx, title, filters)
+	return args.Get(0).([]*domain.Task), args.Get(1).(domain.Metadata), args.Error(2)
 }
 
 // GetByID gets the task when id is 1, otherwise it returns nil, and an ErrRecordNotFound error.
 func (m *MockTaskUsecase) GetByID(ctx context.Context, id int64) (*domain.Task, error) {
-	// Placeholder for context.
-	_ = ctx
-	if id != 1 {
-		return nil, domain.ErrRecordNotFound
-	}
-	task := &domain.Task{
-		ID:      1,
-		Title:   "Do homework",
-		Content: "Interesting",
-		Done:    true,
-		Version: 1,
-	}
-
-	return task, nil
+	args := m.Called(ctx, id)
+	return args.Get(0).(*domain.Task), args.Error(1)
 }
 
 // Insert inserts the task with id = 2
 func (m *MockTaskUsecase) Insert(ctx context.Context, task *domain.Task) error {
-	// Placeholder for context.
-	_ = ctx
-	_ = task
-
-	return nil
+	args := m.Called(ctx, task)
+	return args.Error(0)
 }
 
 // Update updates the task where id = 1, change the following fields:
 // Content: boring, Done: true, and increment Version field to 2.
 func (m *MockTaskUsecase) Update(ctx context.Context, id int64, task *domain.Task) (*domain.Task, error) {
-	// Placeholder for context.
-	_ = ctx
-	_ = task
-
-	task = &domain.Task{
-		ID:      1,
-		Title:   "Do homework",
-		Content: "boring",
-		Done:    true,
-		Version: 2,
-	}
-	return task, nil
+	args := m.Called(ctx, id, task)
+	return args.Get(0).(*domain.Task), args.Error(1)
 }
 
 func (m *MockTaskUsecase) Delete(ctx context.Context, id int64) error {
-	return nil
+	args := m.Called(ctx, id)
+	return args.Error(0)
 }
