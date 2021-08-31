5e13b18c2aef795f850910bd6c4749f18fbd069f
Chang Chen Chien <e850506@gmail.com>
2021-08-31 19:41:38 +0800

    Add code for token usecase

--- internal/token/usecase/token_usecase.go
+++ internal/token/usecase/token_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
@@ -20,11 +21,37 @@ func NewTokenUsecase(tr domain.TokenRepository, timeout time.Duration) domain.To
 }
 
 func (tu *tokenUsecase) New(ctx context.Context, userID int64, ttl time.Duration, scope string) (*domain.Token, error) {
-	return nil, nil
+	ctx, cancel := context.WithTimeout(ctx, tu.contextTimeout)
+	defer cancel()
+
+	token, err := tu.tr.New(ctx, userID, ttl, scope)
+	if err != nil {
+		// TODO: Improve error message chain.
+		return nil, fmt.Errorf("fail to create new token at token usecase: %w", err)
+	}
+	return token, nil
 }
+
 func (tu *tokenUsecase) Insert(ctx context.Context, token *domain.Token) error {
+	ctx, cancel := context.WithTimeout(ctx, tu.contextTimeout)
+	defer cancel()
+	err := tu.tr.Insert(ctx, token)
+	if err != nil {
+		// TODO: Improve error message chain.
+		return fmt.Errorf("fail to insert token at token usecase: %w", err)
+	}
+
 	return nil
 }
 func (tu *tokenUsecase) DeleteAllForUser(ctx context.Context, scope string, userID int64) error {
+	ctx, cancel := context.WithTimeout(ctx, tu.contextTimeout)
+	defer cancel()
+
+	err := tu.tr.DeleteAllForUser(ctx, scope, userID)
+	if err != nil {
+		// TODO: Improve error message chain.
+		return fmt.Errorf("fail to delete all token for user %d at token usecase: %w", userID, err)
+	}
+
 	return nil
 }
