a8ca031d702b37a0e2fc77e1b0b754b7d41664d3
Chang Chen Chien <e850506@gmail.com>
2021-09-01 15:57:33 +0800

    Add test on success call to Insert at token usecase

--- internal/domain/mock/token_repo.go
+++ internal/domain/mock/token_repo.go
@@ -5,22 +5,25 @@ import (
 	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
+
+	"github.com/stretchr/testify/mock"
 )
 
-type MockTokenRepo struct{}
-
-func NewTokenRepo() domain.TokenRepository {
-	return &MockTokenRepo{}
+type MockTokenRepo struct {
+	mock.Mock
 }
 
 func (m *MockTokenRepo) New(ctx context.Context, userID int64, ttl time.Duration, scope string) (*domain.Token, error) {
-	return nil, nil
+	args := m.Called(ctx, userID, ttl, scope)
+	return args.Get(0).(*domain.Token), args.Error(1)
 }
 
 func (m *MockTokenRepo) Insert(ctx context.Context, token *domain.Token) error {
-	return nil
+	args := m.Called(ctx, token)
+	return args.Error(0)
 }
 
 func (m *MockTokenRepo) DeleteAllForUser(ctx context.Context, scope string, userID int64) error {
-	return nil
+	args := m.Called(ctx, scope, userID)
+	return args.Error(0)
 }
--- internal/token/repository/postgres/token_repo.go
+++ internal/token/repository/postgres/token_repo.go
@@ -2,10 +2,7 @@ package postgres
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/sha256"
 	"database/sql"
-	"encoding/base32"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
@@ -22,7 +19,7 @@ func NewTokenRepo(DB *sql.DB) domain.TokenRepository {
 // New is a shortcut which creates a new Token struct and then inserts the
 // data in the tokens table.
 func (tr *tokenRepo) New(ctx context.Context, userID int64, ttl time.Duration, scope string) (*domain.Token, error) {
-	token, err := generateToken(userID, ttl, scope)
+	token, err := domain.GenerateToken(userID, ttl, scope)
 	if err != nil {
 		return nil, err
 	}
@@ -57,43 +54,4 @@ func (tr *tokenRepo) DeleteAllForUser(ctx context.Context, scope string, userID 
 
 	_, err := tr.DB.ExecContext(ctx, query, scope, userID)
 	return err
-}
-
-// generateToken generates token based on userID, ttl (time-to-live), and scope.
-func generateToken(userID int64, ttl time.Duration, scope string) (*domain.Token, error) {
-	token := &domain.Token{
-		UserID: userID,
-		Expiry: time.Now().Add(ttl),
-		Scope:  scope,
-	}
-
-	randomBytes := make([]byte, 16)
-
-	// Use the Read() function from the crypto/rand package to fill the byte slice with
-	// random bytes from your operating system's CSPRNG. This will return an error if
-	// the CSPRNG fails to function correctly.
-	_, err := rand.Read(randomBytes)
-	if err != nil {
-		return nil, err
-	}
-
-	// Encode the byte slice to a base-32-encoded string and assign it to the token
-	// Plaintext field. This will be the token string that we send to the user in their
-	// welcome email. They will look similar to this:
-	//
-	// Y3QMGX3PJ3WLRL2YRTQGQ6KRHU
-	//
-	// Note that by default base-32 strings may be padded at the end with the =
-	// character. We don't need this padding character for the purpose of our tokens, so
-	// we use the WithPadding(base32.NoPadding) method in the line below to omit them.
-	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
-
-	// Generate a SHA-256 hash of the plaintext token string. This will be the value
-	// that we store in the `hash` field of our database table. Note that the
-	// sha256.Sum256() function returns an *array* of length 32, so to make it easier to
-	// work with we convert it to a slice using the [:] operator before storing it.
-	hash := sha256.Sum256([]byte(token.Plaintext))
-	token.Hash = hash[:]
-
-	return token, nil
 }
