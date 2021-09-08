bdd5a6cb9e6b3403548f76712d9008011353be79
Chang Chen Chien <e850506@gmail.com>
2021-09-08 09:35:31 +0800

    Correct wrong test in token usecase - Insert - success
    In previous test,  we set expectation to mock repo,
    and call mockRepo.Insert() directly for test.
    
    But this is wrong, we are in usecase layer, we have to call
    
    err := usecase.Insert(mockRepo)
    
    with mock repo passed in, then make sure mockRepo.Insert()  is called
    as expected, then make sure the return value of usecase.Insert is
    what we expected.

--- internal/token/usecase/token_usecase_test.go
+++ internal/token/usecase/token_usecase_test.go
@@ -2,50 +2,42 @@ package usecase
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
-	"github.com/unknowntpo/todos/internal/domain/mock"
+	_repoMock "github.com/unknowntpo/todos/internal/domain/mock"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestInsert(t *testing.T) {
+	// When success, it should return no error.
 	t.Run("Success", func(t *testing.T) {
-		repo := new(mock.MockTokenRepo)
-		ctx := context.TODO()
+		repo := new(_repoMock.MockTokenRepo)
 		token, err := domain.GenerateToken(1, 30*time.Minute, domain.ScopeActivation)
 		assert.NoError(t, err)
 
-		repo.On("Insert", ctx, token).Return(nil)
+		ctx := context.TODO()
+		repo.On("Insert", mock.Anything, token).Return(nil)
 
-		err = repo.Insert(ctx, token)
+		tokenUsecase := NewTokenUsecase(repo, 3*time.Second)
+		err = tokenUsecase.Insert(ctx, token)
 		assert.NoError(t, err)
 
 		repo.AssertExpectations(t)
 	})
 
-	t.Run("Timeout", func(t *testing.T) {
-		repo := new(mock.MockTokenRepo)
-		ctx := context.TODO()
-		token, err := domain.GenerateToken(1, 30*time.Minute, domain.ScopeActivation)
-		assert.NoError(t, err)
-
-		// Simulate the deadline exceeded context.
-		ctx, cancel := context.WithDeadline(ctx, time.Now().Add(-7*time.Minute))
-		defer cancel()
-
-		wantErr := fmt.Errorf("failed to insert token at token usecase: context deadline exceeded")
-		repo.On("Insert", ctx, token).Return(wantErr)
-
-		gotErr := repo.Insert(ctx, token)
-		assert.Equal(t, wantErr, gotErr)
-
-		repo.AssertExpectations(t)
+	// When failed on db timeout, it sould return with following errors.
+	t.Run("Fail on db timeout", func(t *testing.T) {
+		t.Fail()
 	})
 
+	// When failed on nil token, it sould return with nil token errors.
+	t.Run("Fail on nil token", func(t *testing.T) {
+		t.Fail()
+	})
 }
 
 func TestDeleteAllForUser(t *testing.T) {
