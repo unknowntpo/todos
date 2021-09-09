f8ca1bdf1106802ba817042f65e3c0ce7f2eb8b9
Chang Chen Chien <e850506@gmail.com>
2021-09-10 03:26:40 +0800

    Use pkg/errors.WithMessage to add context to error

--- internal/user/usecase/user_usecase.go
+++ internal/user/usecase/user_usecase.go
@@ -2,10 +2,11 @@ package usecase
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
+
+	"github.com/pkg/errors"
 )
 
 type userUsecase struct {
@@ -24,7 +25,7 @@ func (uu *userUsecase) GetByEmail(ctx context.Context, email string) (*domain.Us
 	user, err := uu.userRepo.GetByEmail(ctx, email)
 	if err != nil {
 		// TODO: Improve error chain message
-		return nil, fmt.Errorf("fail to get user by email at user usecase: %w", err)
+		return nil, errors.WithMessage(err, "user.usecase.GetByEmail")
 	}
 	return user, nil
 }
@@ -35,7 +36,7 @@ func (uu *userUsecase) Insert(ctx context.Context, user *domain.User) error {
 	err := uu.userRepo.Insert(ctx, user)
 	if err != nil {
 		// TODO: Improve error chain message
-		return fmt.Errorf("fail to insert user at user usecase: %w", err)
+		return errors.WithMessage(err, "user.usecase.Insert")
 	}
 	return nil
 }
@@ -46,7 +47,7 @@ func (uu *userUsecase) GetForToken(ctx context.Context, tokenScope, tokenPlainte
 	user, err := uu.userRepo.GetForToken(ctx, tokenScope, tokenPlaintext)
 	if err != nil {
 		// TODO: Improve error chain message
-		return nil, fmt.Errorf("fail to get user for given token at user usecase: %w", err)
+		return nil, errors.WithMessage(err, "user.usecase.GetForToken")
 	}
 	return user, nil
 }
@@ -57,7 +58,7 @@ func (uu *userUsecase) Update(ctx context.Context, user *domain.User) error {
 	err := uu.userRepo.Update(ctx, user)
 	if err != nil {
 		// TODO: Improve error chain message
-		return fmt.Errorf("fail to update user at user usecase: %w", err)
+		return errors.WithMessage(err, "user.usecase.Update")
 	}
 	return nil
 }
