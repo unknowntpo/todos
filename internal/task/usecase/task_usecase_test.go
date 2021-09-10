9a583b28d9521492c4a2c9f929d89c6bcb569f78
Chang Chen Chien <e850506@gmail.com>
2021-09-10 15:32:37 +0800

    Scaffolding tests in task usecase

--- internal/task/usecase/task_usecase_test.go
+++ internal/task/usecase/task_usecase_test.go
@@ -4,6 +4,22 @@ import (
 	"testing"
 )
 
+func TestGetAll(t *testing.T) {
+	t.Fail()
+}
+
 func TestGetByID(t *testing.T) {
+	t.Fail()
+}
+
+func TestInsert(t *testing.T) {
+	t.Fail()
+}
+
+func TestUpdate(t *testing.T) {
+	t.Fail()
+}
+
+func TestDelete(t *testing.T) {
 	t.Fail()
 }
