91700ab42e05db46ccf780560e0dffcf847a8c99
Chang Chen Chien <e850506@gmail.com>
2021-09-11 09:31:13 +0800

    Add test: token repo.Insert with deadline exceeded

--- internal/token/repository/postgres/token_repo_test.go
+++ internal/token/repository/postgres/token_repo_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/unknowntpo/todos/internal/testutil"
 
 	"github.com/golang-migrate/migrate/v4"
-	//"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 )
@@ -20,6 +19,7 @@ type RepoTestSuite struct {
 	container testcontainers.Container
 	db        *sql.DB
 	mig       *migrate.Migrate
+	fakeuser  *domain.User
 }
 
 func (suite *RepoTestSuite) SetupSuite() {
@@ -53,6 +53,31 @@ func (suite *RepoTestSuite) SetupTest() {
 	if err != nil {
 		suite.T().Fatal(err)
 	}
+
+	// Create fake user
+	query := `
+	INSERT INTO users (name, email, password_hash, activated)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id`
+
+	args := []interface{}{
+		"Alice Smith",
+		"alice@example.com",
+		`\x24326124313224765a682f57676e6246446c4f696757654d62616849756874642f3363526b75576558655469782e374f71524c372e78746570635479`,
+		true,
+	}
+
+	// Insert the fake user into user table.
+	var user domain.User
+
+	ctx := context.TODO()
+	err = suite.db.QueryRowContext(ctx, query, args...).Scan(&user.ID)
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+
+	// store fake user in suite
+	suite.fakeuser = &user
 }
 
 // SetupTest do migration down for each test to ensure the results of
@@ -71,49 +96,43 @@ func TestRepoTestSuite(t *testing.T) {
 }
 
 func (suite *RepoTestSuite) TestInsert() {
-	// Create fake user
-	query := `
-	INSERT INTO users (name, email, password_hash, activated)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id`
+	suite.Run("Success", func() {
+		wantToken, err := domain.GenerateToken(suite.fakeuser.ID, 30*time.Minute, domain.ScopeActivation)
+		if err != nil {
+			suite.T().Fatal("fail to generate token")
+		}
 
-	args := []interface{}{
-		"Alice Smith",
-		"alice@example.com",
-		`\x24326124313224765a682f57676e6246446c4f696757654d62616849756874642f3363526b75576558655469782e374f71524c372e78746570635479`,
-		true,
-	}
+		ctx := context.TODO()
+		repo := NewTokenRepo(suite.db)
+		err = repo.Insert(ctx, wantToken)
+		suite.NoError(err)
 
-	// Insert the fake user into user table.
-	var user domain.User
+		// do db query to get inserted token
+		var gotToken domain.Token
+		query := `SELECT hash, user_id, expiry, scope FROM tokens WHERE user_id = $1`
+		err = suite.db.QueryRowContext(ctx, query, suite.fakeuser.ID).Scan(&gotToken.Hash, &gotToken.UserID, &gotToken.Expiry, &gotToken.Scope)
+		suite.NoError(err)
 
-	ctx := context.TODO()
-	err := suite.db.QueryRowContext(ctx, query, args...).Scan(&user.ID)
-	if err != nil {
-		suite.T().Fatal(err)
-	}
+		// We don't care about Expiry, we just make sure that hash, userid,
+		// scope are the same, them we can say that these two token are equal.
+		suite.Equal(wantToken.Hash, gotToken.Hash, "should be equal")
+		suite.Equal(wantToken.UserID, gotToken.UserID, "should be equal")
+		suite.Equal(wantToken.Scope, gotToken.Scope, "should be equal")
+	})
 
-	// Test Insert
-	wantToken, err := domain.GenerateToken(user.ID, 30*time.Minute, domain.ScopeActivation)
-	if err != nil {
-		suite.T().Fatal("fail to generate token")
-	}
+	suite.Run("Fail on timeout", func() {
+		// Create a deadline-exceeded context
+		ctx, cancel := context.WithTimeout(context.Background(), -7*time.Minute)
+		defer cancel()
+		token, err := domain.GenerateToken(suite.fakeuser.ID, 30*time.Minute, domain.ScopeActivation)
+		if err != nil {
+			suite.T().Fatal("fail to generate token")
+		}
 
-	repo := NewTokenRepo(suite.db)
-	err = repo.Insert(ctx, wantToken)
-	suite.NoError(err)
-
-	// do db query to get inserted token
-	var gotToken domain.Token
-	query = `SELECT hash, user_id, expiry, scope FROM tokens WHERE user_id = $1`
-	err = suite.db.QueryRowContext(ctx, query, user.ID).Scan(&gotToken.Hash, &gotToken.UserID, &gotToken.Expiry, &gotToken.Scope)
-	suite.NoError(err)
-
-	// We don't care about Expiry, we just make sure that hash, userid,
-	// scope are the same, them we can say that these two token are equal.
-	suite.Equal(wantToken.Hash, gotToken.Hash, "should be equal")
-	suite.Equal(wantToken.UserID, gotToken.UserID, "should be equal")
-	suite.Equal(wantToken.Scope, gotToken.Scope, "should be equal")
+		repo := NewTokenRepo(suite.db)
+		err = repo.Insert(ctx, token)
+		suite.ErrorIs(err, context.DeadlineExceeded)
+	})
 }
 
 func (suite *RepoTestSuite) TestDeleteAllForUser() {
