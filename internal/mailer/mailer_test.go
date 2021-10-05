6e15cda0b9d28b7b2ce1baf087dba89acf2ad88a
Chang Chen Chien <e850506@gmail.com>
2021-10-05 10:06:50 +0800

    Scaffolding mailer test

---
+++ internal/mailer/mailer_test.go
@@ -0,0 +1,15 @@
+package mailer
+
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"testing"
+	"time"
+)
+
+func TestSend(t *testing.T) {
+	t.Run("send welcome email", func(t *testing.T) {
+
+	})
+}
