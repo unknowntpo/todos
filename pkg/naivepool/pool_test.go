fe4d01843febf68b727101047a2adb180ab79648
Chang Chen Chien <e850506@gmail.com>
2021-10-17 21:51:09 +0800

    Tweak benchmark to avoid memory allocation
    $ go test -v ./pkg/naivepool -benchmem -bench=. -benchtime=3s \
    -cpuprofile cpu.pprof \
    -memprofile mem.pprof
    goos: darwin
    goarch: amd64
    pkg: github.com/unknowntpo/todos/pkg/naivepool
    cpu: Intel(R) Core(TM) i5-7267U CPU @ 3.10GHz
    BenchmarkExecute1000Tasks
    BenchmarkExecute1000Tasks/naivepool
    BenchmarkExecute1000Tasks/naivepool-4         	    3087	   1110369 ns/op	      12 B/op	       0 allocs/op
    BenchmarkExecute1000Tasks/native_goroutine
    BenchmarkExecute1000Tasks/native_goroutine-4  	   14974	    251373 ns/op	       1 B/op	       0 allocs/op
    PASS
    ok  	github.com/unknowntpo/todos/pkg/naivepool	12.445s

--- pkg/naivepool/pool_test.go
+++ pkg/naivepool/pool_test.go
@@ -7,49 +7,58 @@ import (
 	"testing"
 )
 
-func BenchmarkExecute10000Tasks(b *testing.B) {
+func BenchmarkExecute1000Tasks(b *testing.B) {
 	maxJobs := 1000
-	maxWorkers := 1000
+	maxWorkers := 50
 
-	pool := New(maxJobs, maxWorkers)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	pool.Start(ctx)
 	b.Run("naivepool", func(b *testing.B) {
+		pool := New(maxJobs, maxWorkers)
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		pool.Start(ctx)
+
 		var counter uint32
 		var wg sync.WaitGroup
 
+		fn := func() {
+			defer wg.Done()
+			atomic.AddUint32(&counter, 1)
+		}
+
 		b.ResetTimer()
+		b.StartTimer()
 
 		for i := 0; i < b.N; i++ {
 			counter = 0
 			for j := 0; j < maxJobs; j++ {
 				wg.Add(1)
-				pool.Schedule(func() {
-					defer wg.Done()
-					atomic.AddUint32(&counter, 1)
-				})
+				pool.Schedule(fn)
 			}
 			wg.Wait()
 		}
+		b.StopTimer()
 	})
 	b.Run("native goroutine", func(b *testing.B) {
 		var counter uint32
 		var wg sync.WaitGroup
 
+		fn := func() {
+			defer wg.Done()
+			atomic.AddUint32(&counter, 1)
+		}
+
 		b.ResetTimer()
+		b.StartTimer()
 
 		for i := 0; i < b.N; i++ {
 			counter = 0
 			for j := 0; j < maxJobs; j++ {
 				wg.Add(1)
-				go func() {
-					defer wg.Done()
-					atomic.AddUint32(&counter, 1)
-				}()
+				go fn()
 			}
 			wg.Wait()
 		}
+		b.StopTimer()
 	})
 }
