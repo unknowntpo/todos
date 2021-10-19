65a9829d371bc93ffe422cb3c7027814ea61a399
Chang Chen Chien <e850506@gmail.com>
2021-10-19 08:27:00 +0800

    Remove unnecessary mock method in tokenRepo

--- internal/domain/mock/token_repo.go
+++ internal/domain/mock/token_repo.go
@@ -2,7 +2,6 @@ package mock
 
 import (
 	"context"
-	"time"
 
 	"github.com/unknowntpo/todos/internal/domain"
 
@@ -11,11 +10,6 @@ import (
 
 type MockTokenRepo struct {
 	mock.Mock
-}
-
-func (m *MockTokenRepo) New(ctx context.Context, userID int64, ttl time.Duration, scope string) (*domain.Token, error) {
-	args := m.Called(ctx, userID, ttl, scope)
-	return args.Get(0).(*domain.Token), args.Error(1)
 }
 
 func (m *MockTokenRepo) Insert(ctx context.Context, token *domain.Token) error {
