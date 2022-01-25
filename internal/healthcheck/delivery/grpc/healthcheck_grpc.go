b84861894ceddd5bbc45252524b7891505173ec5
Chang Chen Chien <e850506@gmail.com>
2022-01-25 09:31:01 +0800

    Use pb as package alias

--- internal/healthcheck/delivery/grpc/healthcheck_grpc.go
+++ internal/healthcheck/delivery/grpc/healthcheck_grpc.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/unknowntpo/todos/internal/healthcheck/delivery/grpc/healthcheck"
+	pb "github.com/unknowntpo/todos/internal/healthcheck/delivery/grpc/healthcheck"
 	"github.com/unknowntpo/todos/internal/logger"
 
 	"google.golang.org/grpc"
 )
 
 type healthcheckGrpcServer struct {
-	healthcheck.UnimplementedHealthcheckServer
+	pb.UnimplementedHealthcheckServer
 	logger  logger.Logger
 	env     string
 	version string
@@ -19,14 +19,14 @@ type healthcheckGrpcServer struct {
 
 func NewHealthcheckGrpc(srv *grpc.Server, env, version string, l logger.Logger) {
 	h := &healthcheckGrpcServer{}
-	healthcheck.RegisterHealthcheckServer(srv, h)
+	pb.RegisterHealthcheckServer(srv, h)
 }
 
 // Healthcheck returns the status of our service.
-func (h *healthcheckGrpcServer) Healthcheck(ctx context.Context, r *healthcheck.HealthcheckRequest) (*healthcheck.HealthcheckResponse, error) {
+func (h *healthcheckGrpcServer) Healthcheck(ctx context.Context, r *pb.HealthcheckRequest) (*pb.HealthcheckResponse, error) {
 	h.logger.PrintInfo(fmt.Sprintf("healthcheckRequest: %s", r.String()), nil)
 
-	resp := &healthcheck.HealthcheckResponse{
+	resp := &pb.HealthcheckResponse{
 		Environment: h.env,
 		Status:      "available",
 		Version:     h.version,
